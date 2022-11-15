db3b99d9a281dcfde7d3aec86ea4ef17a2f39de9
Lull3n <61215087+Lull3n@users.noreply.github.com>
2022-11-15 15:48:23 +0100

    Update main.go

--- main.go
+++ main.go
@@ -4,21 +4,19 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
-	"io"
 	"net"
 	"net/http"
 	"os"
 )
 
-const SERVER_PORT = "8080"
-
 func main() {
-	startServer()
+	port := os.Args[1]
+	startServer(port)
 }
 
-func startServer() {
-	fmt.Print("Server starting on port: " + SERVER_PORT)
-	server, err := net.Listen("tcp", "localhost:"+SERVER_PORT)
+func startServer(port string) {
+	fmt.Print("Server starting on port: " + port)
+	server, err := net.Listen("tcp", "localhost:"+port)
 	if err != nil {
 		fmt.Print("Error starting server: " + err.Error())
 		os.Exit(-1)
@@ -35,31 +33,35 @@ func startServer() {
 }
 
 func processClient(client net.Conn) {
+	println("\nNew client accepted, processing request.")
 	buf := make([]byte, 0) // do not use.
-	tmp := make([]byte, 1024)
+	tmp := make([]byte, 10048)
 	buffer := bytes.NewBuffer(buf)
 	readLen, err := client.Read(tmp)
-	for err == nil && readLen != -1 {
-		buffer.Write(tmp[:readLen])
-		fmt.Print("Read: ")
-		fmt.Println(tmp[:readLen])
-		break
-		//readLen, err = client.Read(tmp)
+	if err != nil {
+		fmt.Println("Error")
 	}
+	//for err == nil && readLen != -1 {
+	buffer.Write(tmp[:readLen])
+	//fmt.Print("Read: ")
+	//fmt.Println(tmp[:readLen])
+	//	break
+	//readLen, err = client.Read(tmp)
+	//}
 
 	reader := bufio.NewReader(buffer)
 	req, err := http.ReadRequest(reader)
+
+	println("URL: " + req.URL.Path)
 	fmt.Println("Got request: " + req.Method)
-	if req.Method != "GET" {
+	if req.Method == "GET" { // handle get
+
+	} else if req.Method == "POST" { //handle post
+
+	} else {
 		var res = http.Response{Close: true, StatusCode: 501}
+		res.Write(client)
 
-		i := 1
-		mybytes, err := io.ReadAll(res.Body)
-
-		if err != nil {
-			fmt.Println("Error reading res body for response")
-		}
-		client.Write(mybytes)
 		req.Close = true
 	}
 }
