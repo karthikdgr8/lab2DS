a35ca61131e623a5fdac29c87c7881f8f873fd49
Lull3n <61215087+Lull3n@users.noreply.github.com>
2022-12-01 15:20:27 +0100

    Network

--- src/chords/main.go
+++ src/chords/main.go
@@ -1,6 +1,12 @@
 package main
 
+import "time"
+
 func main() {
 	// parse arguments
+	createInstance("127.0.0.1", "12323")
+	start()
+	print("Sleeping")
 
+	time.Sleep(100 * time.Millisecond)
 }
---
+++ src/chords/net.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"encoding/json"
+	"log"
+	"net"
+)
+
+type Client struct {
+	Ip   string
+	Port string
+	ID   string
+}
+
+type MessageType struct {
+	Action string
+	Owner  Client
+	Vars   []string
+}
+
+var IP string
+var PORT string
+var DELIM = 0x1F
+
+func createInstance(ip string, port string) {
+	IP = ip
+	PORT = port
+}
+
+func start() {
+	log.Println("STARTING TCP-SERVER")
+	server, err := net.Listen("tcp", IP+":"+PORT)
+	if err != nil {
+		log.Println("Error: " + err.Error())
+	}
+	serverRoutine(server)
+
+}
+
+func serverRoutine(listener net.Listener) {
+	for {
+		client, err := listener.Accept()
+		if err != nil {
+			log.Println("ERROR ACCEPTING CLIENT: " + err.Error())
+		} else {
+			go handleNewConnection(client)
+		}
+	}
+}
+
+func listenForData(conn net.Conn) []byte {
+	log.Println("New client accepted")
+	connBuf := bufio.NewReader(conn)
+	data, err := connBuf.ReadBytes(byte(DELIM))
+	data = data[:len(data)-1]
+	if err != nil {
+		log.Println("ERROR READING DATA: " + err.Error())
+		return nil
+	}
+	return data
+}
+
+func connectToPeer(ip string, port string) net.Conn {
+	conn, err := net.Dial("tcp", ip+":"+port)
+	if err != nil {
+		log.Println("ERROR ESTABLISHING CLINET TCP: " + err.Error())
+		return nil
+	}
+	return conn
+}
+
+func clientSend(conn net.Conn, data []byte) {
+	_, err := conn.Write(append(data, byte(DELIM)))
+	if err != nil {
+		log.Println("ERROR SENDING DATA: " + err.Error())
+	}
+}
+
+//Handle new client.
+func handleNewConnection(conn net.Conn) {
+
+	data := listenForData(conn)
+	message := map[string]MessageType{}
+	err := json.Unmarshal(data, &message)
+	if (err != nil) {
+		log.Println("ERROR UNMARSHALLING MESSAGE: ", err.Error())
+	}
+
+}
