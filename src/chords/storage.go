e6ef10106279aa8013bdfe362bfdc5cc5375f8d5
Karthik R Sharma <karthik.r@wrizto.com>
2022-12-05 17:22:30 +0100

    Storage basics

--- src/chords/storage.go
+++ src/chords/storage.go
@@ -1,1 +1,47 @@
 package main
+
+import (
+	"bytes"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+var filePath = "/Users/karthik/Public/tmp/"
+
+func storeFile(fileName string, conn net.Conn) {
+
+	data := listenForData(conn)
+
+	dataReader := bytes.NewReader(data)
+
+	d, err := os.Create(filePath + fileName) // Create a file in the path with given file name on the filesystem
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	defer func(d *os.File) {
+		err := d.Close()
+		if err != nil {
+			return
+		}
+	}(d)
+
+	_, err = io.Copy(d, dataReader) // Copy the file from request to the newly created file on the the file system
+	if err != nil {
+		return
+	}
+}
+
+func searchFile(fileName string) []byte {
+
+	file, err := os.ReadFile(filePath + "" + fileName + "")
+	if err != nil {
+		log.Println("Error reading file or file not found" + err.Error())
+		return nil
+	}
+
+	return file
+}
